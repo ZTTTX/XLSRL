a1bcae2a08e39c8196b0b6785db42db5eabe585f
TZ <907241061@qq.com>
2023-11-20 20:47:49 -0800

    native_pick selector is under construction

--- work_space/Sha256/sha256.opt.v
+++ work_space/Sha256/sha256.opt.v
@@ -4,13 +4,21 @@ module xls_test(
   output wire [29:0] out
 );
   // ===== Pipe stage 0:
-  wire [29:0] p0_literal_51817_comb;
-  assign p0_literal_51817_comb = 30'h0eb1_0b89;
 
   // Registers for pipe stage 0:
-  reg [29:0] p1_literal_51817;
+  reg [511:0] p0_message;
   always_ff @ (posedge clk) begin
-    p1_literal_51817 <= p0_literal_51817_comb;
+    p0_message <= message;
   end
-  assign out = p1_literal_51817;
+
+  // ===== Pipe stage 1:
+  wire [29:0] p1_add_51670_comb;
+  assign p1_add_51670_comb = p0_message[415:386] + 30'h0eb1_0b89;
+
+  // Registers for pipe stage 1:
+  reg [29:0] p1_add_51670;
+  always_ff @ (posedge clk) begin
+    p1_add_51670 <= p1_add_51670_comb;
+  end
+  assign out = p1_add_51670;
 endmodule
